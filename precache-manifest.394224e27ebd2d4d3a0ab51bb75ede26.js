9bc6f77a235d12edf416989e1c06c014e4a8d5e8
EstherGodoy <info@esthergodoy.com>
2019-09-10 17:38:54 -0700

    Updates

---
+++ precache-manifest.394224e27ebd2d4d3a0ab51bb75ede26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9770396bcaa894341178645eac5d776",
+    "url": "/smart-recruiter/index.html"
+  },
+  {
+    "revision": "afcc7e381dd86f59707e",
+    "url": "/smart-recruiter/static/css/main.b341216d.chunk.css"
+  },
+  {
+    "revision": "af1998806d660569fea4",
+    "url": "/smart-recruiter/static/js/2.527a5cc2.chunk.js"
+  },
+  {
+    "revision": "afcc7e381dd86f59707e",
+    "url": "/smart-recruiter/static/js/main.5d10f618.chunk.js"
+  },
+  {
+    "revision": "5e649535af4a1acf58f5",
+    "url": "/smart-recruiter/static/js/runtime~main.d6d5f692.js"
+  }
+]);
